ec10b142b25ce55e7df71fb757db992b97ead4de
houarizegai <houarizegai14@gmail.com>
2018-09-17 13:43:07 +0200

    Upload project

---
+++ src/com/houarizegai/generatetablefx/java/controllers/MainController.java
@@ -0,0 +1,19 @@
+package com.houarizegai.generatetablefx.java.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private VBox columnsBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
