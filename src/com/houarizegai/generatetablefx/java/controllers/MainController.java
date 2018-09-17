f1413aa741bb5e5ff0ef5d35cb4a31f017e3db6c
houarizegai <houarizegai14@gmail.com>
2018-09-17 16:49:03 +0200

    Adding Column & Automate some activity

--- src/com/houarizegai/generatetablefx/java/controllers/MainController.java
+++ src/com/houarizegai/generatetablefx/java/controllers/MainController.java
@@ -2,15 +2,20 @@ package com.houarizegai.generatetablefx.java.controllers;
 
 import com.houarizegai.generatetablefx.java.models.ColumnTable;
 import com.houarizegai.generatetablefx.java.models.TableInfo;
+import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTreeTableView;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -21,7 +26,10 @@ public class MainController implements Initializable {
     private JFXTextField fieldTitleTable;
 
     @FXML // This Box contain Boxs of Columns
-    private VBox columnsBox;
+    private JFXListView columnsBox;
+
+    @FXML
+    private Text txtCounterCol;
 
     @FXML
     private TextArea resultArea;
@@ -31,15 +39,15 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        onAddColumn();
     }
 
     private void extractDataOfTable() {
 
-        columnsTable = new ColumnTable[columnsBox.getChildren().size()];
+        columnsTable = new ColumnTable[columnsBox.getItems().size()];
 
         for(int i = 0; i < columnsTable.length; i++) {
-            HBox hBox = (HBox) columnsBox.getChildren().get(i);
+            HBox hBox = (HBox) columnsBox.getItems().get(i);
             columnsTable[i] = new ColumnTable();
             columnsTable[i].setTitle(((JFXTextField) hBox.getChildren().get(1)).getText().trim());
             columnsTable[i].setVarName("col" + columnsTable[i].getTitle().substring(0, 1).toUpperCase()
@@ -94,10 +102,36 @@ public class MainController implements Initializable {
             result.append("\n");
         }
 
-        
+        double tableWidth = 0d;
+
+        result.append("\n\t" + tableInfo.getVarName() + ".getColumns().addAll(");
+        StringBuilder cols = new StringBuilder();
+        for(ColumnTable col : columnsTable) {
+            cols.append(col.getVarName() + ", ");
+            tableWidth += Double.parseDouble(col.getWidth());
+        }
+        result.append(cols.substring(0, cols.length() - 2) + ");\n");
+        result.append("\t" + tableInfo.getVarName() + ".setPrefWidth(" + tableWidth + ");\n");
+        result.append("\t" + tableInfo.getVarName() + ".setShowRoot(false);\n");
 
         result.append("}");
+
+        JFXTreeTableView t = new JFXTreeTableView();
+
         return result.toString();
+
+    }
+
+    @FXML
+    private void onAddColumn() {
+        HBox colBox = null;
+        try {
+            colBox = FXMLLoader.load(getClass().getResource("/com/houarizegai/generatetablefx/resources/views/ColumnBox.fxml"));
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+        columnsBox.getItems().add(colBox);
+        txtCounterCol.setText(String.valueOf(columnsBox.getItems().size()));
     }
 
     @FXML
